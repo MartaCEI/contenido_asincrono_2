f30c95c91d4ed839e593b2c8066b2b07bdd36a0c
guecolmar <guecolmar@gmail.com>
2024-09-09 12:09:22 +0200

    contenido

---
+++ landing-back/data/mockdata.js
@@ -0,0 +1,76 @@
+export const hero = {
+    img:"",
+    icono: "",
+    color: "",
+    btnCallAction: {
+        img: "",
+        url: "",
+    },
+    titulo: "",
+    contenido: ""
+}
+
+export const sections = [
+    {
+        img:"",
+        icono: "",
+        color: "",
+        titulo: "",
+        contenido: "",
+        isImageRight: false,
+    }
+]
+
+export const aplications = [
+    {
+        icono: "",
+        titulo: ""
+    },
+    {
+        icono: "",
+        titulo: "Works with Gmail, iCloud & IMAP"
+    },
+    {
+        icono: "",
+        titulo: "All procesing happens on device"
+    },
+    {
+        icono: "",
+        titulo: ""
+    },
+    {
+        icono: "",
+        titulo: ""
+    },
+    {
+        icono: "",
+        titulo: ""
+    },
+]
+
+export const clients = [
+    {
+        content:"",
+        logo: ""
+    },
+    {
+        content:"",
+        logo: ""
+    },
+    {
+        content:"",
+        logo: ""
+    },
+    {
+        content:"",
+        logo: ""
+    },
+    {
+        content:"",
+        logo: ""
+    },
+    {
+        content:"",
+        logo: ""
+    },
+]
---
+++ landing-front/src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import { useLayoutEffect } from "react";
+
+
+
+const Home = () => {
+
+    useEffect(() => {
+        
+    }, []);
+
+    const getData = async () => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Home;
